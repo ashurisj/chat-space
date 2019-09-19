87e13585d35378907deaad6d444f23d972e36913
ashurisj <new4s9_asm97@icloud.com>
2019-09-19 17:38:29 +0900

    fixed var

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,25 +1,25 @@
 $(function(){
   function buildHTML(message) {
-      var image = 
-      message.image ? '<img src=$message.image>' : ''  
-      var html =
-        `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.date}
-            </div>
+    var html =
+      `<div class="message" data-message-id=${message.id}>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
           </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
+          <div class="upper-message__date">
+            ${message.date}
           </div>
-        </div>`
-      return html + image;
-  }
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+    var image = 
+    message.image ? `<img src=${message.image}>` : ('')
+    return html + image;
+  } 
     $('form').on('submit', function(e){
       e.preventDefault();
       var formData = new FormData(this);
